f0eb631266c9ec202b51d0a908d0c23e147ead37
xtuskay <festusoluwaseye@gmail.com>
2024-05-13 16:56:20 -0400

    added functionality for launches

--- src/pages/Events/Events.js
+++ src/pages/Events/Events.js
@@ -1,44 +1,201 @@
 
+// import React, { useState, useEffect } from "react";
+// import "./Events.scss";
+
+// function Events() {
+//   const [events, setEvents] = useState([]);
+//   const [selectedEventType, setSelectedEventType] = useState("upcoming");
+
+//   useEffect(() => {
+//     const today = new Date().toISOString().split("T")[0];
+
+//     fetch(
+//       `/events/?date__${
+//         selectedEventType === "upcoming" ? "gte" : "lt"
+//       }=${today}`
+//     )
+//       .then((response) => response.json())
+//       .then((data) => setEvents(data.results))
+//       .catch((error) => console.error("Error:", error));
+//   }, [selectedEventType]);
+
+//   const handleEventTypeChange = (event) => {
+//     setSelectedEventType(event.target.value);
+//   };
+
+//   return (
+//     <div className="events">
+//       <div className="events__filter">
+//         <select
+//           className="events__filter-select"
+//           value={selectedEventType}
+//           onChange={handleEventTypeChange}
+//         >
+//           <option value="upcoming">Upcoming Events</option>
+//           <option value="past">Past Events</option>
+//         </select>
+//       </div>
+//       <div className="events__section">
+//         <h1 className="events__heading">
+//           {selectedEventType === "upcoming" ? "Upcoming" : "Past"} Events
+//         </h1>
+//         <div className="events__card-container">
+//           {events.map((event) => (
+//             <div key={event.id} className="events__card">
+//               <div className="events__image-container">
+//                 <img
+//                   src={event.feature_image}
+//                   alt={event.name}
+//                   className="events__image"
+//                 />
+//               </div>
+//               <div className="events__details">
+//                 <h2 className="events__title">{event.name}</h2>
+//                 <p className="events__description">{event.description}</p>
+//                 <p className="events__date">
+//                   Date: {new Date(event.date).toLocaleString()}
+//                 </p>
+//                 <p className="events__location">Location: {event.location}</p>
+//                 {event.vid_urls.length > 0 && (
+//                   <a
+//                     href={event.vid_urls[0].url}
+//                     className="events__video-link"
+//                   >
+//                     Watch Video
+//                   </a>
+//                 )}
+//               </div>
+//             </div>
+//           ))}
+//         </div>
+//       </div>
+//     </div>
+//   );
+// }
+
+// export default Events;
+
+// import React, { useState, useEffect } from "react";
+// import "./Events.scss";
+
+// function Events() {
+//   const [events, setEvents] = useState([]);
+//   const [selectedEventType, setSelectedEventType] = useState("upcoming");
+
+//   useEffect(() => {
+//     const today = new Date().toISOString().split("T")[0];
+
+//     fetchEvents(today, selectedEventType);
+//   }, [selectedEventType]);
+
+//   const fetchEvents = (date, eventType) => {
+//     const comparisonOperator = eventType === "upcoming" ? "gte" : "lt";
+//     fetch(`/events/?date__${comparisonOperator}=${date}`)
+//       .then((response) => response.json())
+//       .then((data) => setEvents(data.results))
+//       .catch((error) => console.error("Error:", error));
+//   };
+
+//   const handleEventTypeChange = (eventType) => {
+//     setSelectedEventType(eventType);
+//   };
+
+//   return (
+//     <div className="events">
+//       <div className="events__filter">
+//         <select
+//           className="events__filter-select"
+//           value={selectedEventType}
+//           onChange={(e) => handleEventTypeChange(e.target.value)}
+//         >
+//           <option value="upcoming">Upcoming Events</option>
+//           <option value="past">Past Events</option>
+//         </select>
+//       </div>
+//       <div className="events__section">
+//         <h1 className="events__heading">
+//           {selectedEventType === "upcoming" ? "Upcoming" : "Past"} Events
+//         </h1>
+//         <div className="events__card-container">
+//           {events.map((event) => (
+//             <div key={event.id} className="events__card">
+//               <div className="events__image-container">
+//                 <img
+//                   src={event.feature_image}
+//                   alt={event.name}
+//                   className="events__image"
+//                 />
+//               </div>
+//               <div className="events__details">
+//                 <h2 className="events__title">{event.name}</h2>
+//                 <p className="events__description">{event.description}</p>
+//                 <p className="events__date">
+//                   Date: {new Date(event.date).toLocaleString()}
+//                 </p>
+//                 <p className="events__location">{event.location}</p>
+//                 {event.vid_urls.length > 0 && (
+//                   <a
+//                     href={event.vid_urls[0].url}
+//                     className="events__video-link"
+//                   >
+//                     Watch Video
+//                   </a>
+//                 )}
+//               </div>
+//             </div>
+//           ))}
+//         </div>
+//       </div>
+//     </div>
+//   );
+// }
+
+// export default Events;
+
+
+
 import React, { useState, useEffect } from "react";
 import "./Events.scss";
 
 function Events() {
   const [events, setEvents] = useState([]);
-  const [selectedEventType, setSelectedEventType] = useState("upcoming");
+  const [showUpcoming, setShowUpcoming] = useState(true);
 
   useEffect(() => {
     const today = new Date().toISOString().split("T")[0];
+    fetchEvents(today, showUpcoming);
+  }, [showUpcoming]);
 
-    fetch(
-      `/events/?date__${
-        selectedEventType === "upcoming" ? "gte" : "lt"
-      }=${today}`
-    )
+  const fetchEvents = (date, upcoming) => {
+    const comparisonOperator = upcoming ? "gte" : "lt";
+    fetch(`/events/?date__${comparisonOperator}=${date}`)
       .then((response) => response.json())
       .then((data) => setEvents(data.results))
       .catch((error) => console.error("Error:", error));
-  }, [selectedEventType]);
+  };
 
-  const handleEventTypeChange = (event) => {
-    setSelectedEventType(event.target.value);
+  const toggleSection = () => {
+    setShowUpcoming(!showUpcoming);
   };
 
   return (
     <div className="events">
-      <div className="events__filter">
-        <select
-          className="events__filter-select"
-          value={selectedEventType}
-          onChange={handleEventTypeChange}
+      <h1 className="events__heading">Events</h1>
+      <div className="events__toggle">
+        <button
+          className={`events__toggle-button ${showUpcoming ? "active" : ""}`}
+          onClick={toggleSection}
         >
-          <option value="upcoming">Upcoming Events</option>
-          <option value="past">Past Events</option>
-        </select>
+          Upcoming
+        </button>
+        <button
+          className={`events__toggle-button ${!showUpcoming ? "active" : ""}`}
+          onClick={toggleSection}
+        >
+          Past
+        </button>
       </div>
       <div className="events__section">
-        <h1 className="events__heading">
-          {selectedEventType === "upcoming" ? "Upcoming" : "Past"} Events
-        </h1>
         <div className="events__card-container">
           {events.map((event) => (
             <div key={event.id} className="events__card">
@@ -55,7 +212,7 @@ function Events() {
                 <p className="events__date">
                   Date: {new Date(event.date).toLocaleString()}
                 </p>
-                <p className="events__location">Location: {event.location}</p>
+                <p className="events__location">{event.location}</p>
                 {event.vid_urls.length > 0 && (
                   <a
                     href={event.vid_urls[0].url}
@@ -74,4 +231,3 @@ function Events() {
 }
 
 export default Events;
-
--- src/pages/Vehicles/Vehicles.js
+++ src/pages/Vehicles/Vehicles.js
@@ -1,39 +1,172 @@
-import React, { useState, useEffect } from "react";
 
-function Vehicles() {
+// import React, { useState, useEffect } from "react";
+// import axios from "axios";
+
+// function VehiclesPage() {
+//   const [vehicles, setVehicles] = useState([]);
+//   const [showReusable, setShowReusable] = useState(false);
+//   const [selectedVehicle, setSelectedVehicle] = useState(null);
+
+//   useEffect(() => {
+//     fetchVehicles(showReusable);
+//   }, [showReusable]);
+
+//   const fetchVehicles = async (showReusable) => {
+//     try {
+//       const endpoint = showReusable ? "/vehicles/reusable" : "/vehicles";
+//       const response = await axios.get(endpoint);
+//       setVehicles(response.data.results); // Assuming the API response has a 'results' property containing an array of vehicles
+//     } catch (error) {
+//       console.error("Error fetching vehicles:", error);
+//     }
+//   };
+
+//   const toggleSection = () => {
+//     setShowReusable(!showReusable);
+//   };
+
+//   const handleCardClick = (vehicle) => {
+//     setSelectedVehicle(vehicle === selectedVehicle ? null : vehicle);
+//   };
+
+//   return (
+//     <div className="vehicles">
+//       <h1 className="vehicles__heading">Vehicles</h1>
+//       <div className="vehicles__toggle">
+//         <button
+//           className={`vehicles__toggle-button ${showReusable ? "active" : ""}`}
+//           onClick={toggleSection}
+//         >
+//           Reusable
+//         </button>
+//         <button
+//           className={`vehicles__toggle-button ${!showReusable ? "active" : ""}`}
+//           onClick={toggleSection}
+//         >
+//           Non-reusable
+//         </button>
+//       </div>
+//       <div className="vehicles__card-container">
+//         {Array.isArray(vehicles) && vehicles.map((vehicle) => (
+//           <div
+//             key={vehicle.id}
+//             className={`vehicles__card ${
+//               selectedVehicle === vehicle ? "selected" : ""
+//             }`}
+//             onClick={() => handleCardClick(vehicle)}
+//           >
+//             <img
+//               src={vehicle.image_url}
+//               alt={vehicle.serial_number}
+//               className="vehicles__image"
+//             />
+//             <div className="vehicles__info">
+//               <h2 className="vehicles__title">{vehicle.serial_number}</h2>
+//               {selectedVehicle === vehicle && (
+//                 <div>
+//                   <p className="vehicles__details">{vehicle.details}</p>
+//                   <p className="vehicles__status">Status: {vehicle.status}</p>
+//                   <p className="vehicles__flights">Flights: {vehicle.flights}</p>
+//                   <p className="vehicles__first-launch-date">
+//                     First Launch Date:{" "}
+//                     {new Date(vehicle.first_launch_date).toLocaleDateString()}
+//                   </p>
+//                   <p className="vehicles__last-launch-date">
+//                     Last Launch Date:{" "}
+//                     {new Date(vehicle.last_launch_date).toLocaleDateString()}
+//                   </p>
+//                 </div>
+//               )}
+//             </div>
+//           </div>
+//         ))}
+//       </div>
+//     </div>
+//   );
+// }
+
+// export default VehiclesPage;
+
+
+
+
+
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
+function VehiclesPage() {
   const [vehicles, setVehicles] = useState([]);
+  const [showReusable, setShowReusable] = useState(false);
 
   useEffect(() => {
-    fetch("/vehicles")
-      .then((response) => response.json())
-      .then((data) => setVehicles(data.results))
-      .catch((error) => console.error("Error:", error));
-  }, []);
+    fetchVehicles(showReusable);
+  }, [showReusable]);
+
+  const fetchVehicles = async (showReusable) => {
+    try {
+      const endpoint = showReusable ? "/vehicles/reusable" : "/vehicles";
+      const response = await axios.get(endpoint);
+      setVehicles(response.data.results);
+    } catch (error) {
+      console.error("Error fetching vehicles:", error);
+    }
+  };
+
+  const toggleSection = () => {
+    setShowReusable(!showReusable);
+  };
+
+  const toggleCard = (index) => {
+    setVehicles((prevVehicles) =>
+      prevVehicles.map((vehicle, i) =>
+        i === index ? { ...vehicle, expanded: !vehicle.expanded } : vehicle
+      )
+    );
+  };
 
   return (
-    <div className="vehicles__container">
+    <div className="vehicles">
       <h1 className="vehicles__heading">Vehicles</h1>
+      <div className="vehicles__toggle">
+        <button
+          className={`vehicles__toggle-button ${showReusable ? "active" : ""}`}
+          onClick={toggleSection}
+        >
+          Reusable
+        </button>
+        <button
+          className={`vehicles__toggle-button ${!showReusable ? "active" : ""}`}
+          onClick={toggleSection}
+        >
+          Non-reusable
+        </button>
+      </div>
       <div className="vehicles__card-container">
-        {vehicles.map((vehicle) => (
-          <div key={vehicle.id} className="vehicles__card">
-            <img
-              src={vehicle.image_url}
-              alt={vehicle.serial_number}
-              className="vehicles__image"
-            />
+        {vehicles.map((vehicle, index) => (
+          <div
+            key={vehicle.id}
+            className={`vehicles__card ${vehicle.expanded ? "expanded" : ""}`}
+            onClick={() => toggleCard(index)}
+          >
             <div className="vehicles__info">
               <h2 className="vehicles__title">{vehicle.serial_number}</h2>
-              <p className="vehicles__details">{vehicle.details}</p>
-              <p className="vehicles__status">Status: {vehicle.status}</p>
-              <p className="vehicles__flights">Flights: {vehicle.flights}</p>
-              <p className="vehicles__first-launch-date">
-                First Launch Date:{" "}
-                {new Date(vehicle.first_launch_date).toLocaleDateString()}
+              <p className="vehicles__details">
+                {vehicle.expanded ? vehicle.details : "Click to view details"}
               </p>
-              <p className="vehicles__last-launch-date">
-                Last Launch Date:{" "}
-                {new Date(vehicle.last_launch_date).toLocaleDateString()}
-              </p>
+              {vehicle.expanded && (
+                <div>
+                  <p className="vehicles__status">Status: {vehicle.status}</p>
+                  <p className="vehicles__flights">Flights: {vehicle.flights}</p>
+                  <p className="vehicles__first-launch-date">
+                    First Launch Date:{" "}
+                    {new Date(vehicle.first_launch_date).toLocaleDateString()}
+                  </p>
+                  <p className="vehicles__last-launch-date">
+                    Last Launch Date:{" "}
+                    {new Date(vehicle.last_launch_date).toLocaleDateString()}
+                  </p>
+                </div>
+              )}
             </div>
           </div>
         ))}
@@ -42,4 +175,5 @@ function Vehicles() {
   );
 }
 
-export default Vehicles;
+export default VehiclesPage;
+
